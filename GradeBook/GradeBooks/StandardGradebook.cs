862d8cceeda9011c7858d66f3e7e16849e0e79e0
Yvon NChonon <ynchonon@emoneyadvisor.com>
2019-01-23 14:47:29 -0500

    Added a constructor for StandardGradebook

---
+++ GradeBook/GradeBooks/StandardGradebook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradebook : BaseGradeBook
+    {
+        public StandardGradebook(string name):base(name)
+        {
+        }
+    }
+}
