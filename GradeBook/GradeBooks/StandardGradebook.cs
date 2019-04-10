8c66f8de3f5d79152b68243d290114c8705d7845
Yvon NChonon <ynchonon@emoneyadvisor.com>
2019-04-10 08:55:59 -0400

    Add isWeighted parameter and check if gradebook isWeighted is True or False

--- GradeBook/GradeBooks/StandardGradebook.cs
+++ GradeBook/GradeBooks/StandardGradebook.cs
@@ -4,7 +4,7 @@ namespace GradeBook.GradeBooks
 {
     public class StandardGradebook : BaseGradeBook
     {
-        public StandardGradebook(string name):base(name)
+        public StandardGradebook(string name , bool isWeighted) : base(name, isWeighted)
         {
             Type = GradeBookType.Standard;
         }
